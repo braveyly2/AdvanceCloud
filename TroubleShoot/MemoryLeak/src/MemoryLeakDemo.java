20e1127608239b85c1c351e155c5599ae740310a
nick <sjliwei2002@126.com>
2020-12-17 16:53:25 +0800

    troubleshooting增加memoryleak示例及定位方法

---
+++ TroubleShoot/MemoryLeak/src/MemoryLeakDemo.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryLeakDemo {
+
+    /*
+    方法1：
+    java -Xms128m -Xmx128m -XX:+HeapDumpOnOutOfMemoryError -jar MemoryLeak.jar
+    生成dump后，采用jvirsualvm.exe查看和分析
+    方法2：
+    jmap -dump:format=b,file=javaml.hprof 16289
+    手动主动生成dump
+
+    然后用JProfiler来分析
+    * */
+    /*
+    ML Demo 1519770
+    ML Demo 1519771
+    Exception in thread "main" java.lang.OutOfMemoryError: GC overhead limit exceeded
+	at MemoryLeakDemo.main(MemoryLeakDemo.java:10)
+    * */
+    void HeapOOM(){
+        List list = new ArrayList<String>();
+        int i = 0;
+        while(true){
+            list.add(new String("liwei"+i));
+            i++;
+            System.out.println("ML Demo "+i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
